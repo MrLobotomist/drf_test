d2e9d4d1e59793ef76aa1e224c22536b07dab7d0
MrLobotomist <39457028+MrLobotomist@users.noreply.github.com>
2024-09-14 12:25:32 +0500

    init 1

---
+++ server/settings/enviroments/local.py
@@ -0,0 +1,26 @@
+from decouple import Config, Csv
+import os
+from .base import BASE_DIR
+
+# CONFIG LOAD
+env_file_path = os.path.join(BASE_DIR, 'config', '.env')
+print(env_file_path)
+config = Config(env_file_path)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
